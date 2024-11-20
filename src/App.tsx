2ca26ca8a7158e477f6991c3502d131a839117fc
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-20 20:05:11 -0300

    adiciona react routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@
 import './App.css'
 import Footer from './Componentes/Footer/footer'
 import Previsão from './Componentes/Previsão/Previsão'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Rotas from './Componentes/Rotas/Rotas';
+const router = createBrowserRouter([
+  {
+    path: "/cadeCircular",
+    element: <Previsão />,
+  },
+  {
+    path: "/cadeCircular/rotas",
+    element: <Rotas />,
+  }
+]);
 function App() {
 
   return (
+    
     <div className='app'>
-      <Previsão></Previsão>
-      <Footer></Footer>
+        <RouterProvider router={router} />
+        <Footer></Footer>
     </div>
+      
+   
   )
 }
 
