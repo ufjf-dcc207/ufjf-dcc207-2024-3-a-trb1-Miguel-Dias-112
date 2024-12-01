bd497bb279777ff9a0aa140fe4193a6dcc43948f
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-01 11:28:21 -0300

    isola rotas em um arquivo separado

---
+++ src/APIS/Previsões/RotasData.tsx
@@ -0,0 +1,133 @@
+import "./Models.tsx";
+export type Rotas = {
+  nome: string;
+  pontos: Ponto[];
+};
+export type Horario = {
+  nome: string;
+  rotas: Rotas[];
+  intervaloPontos: number;
+  inicio: string;
+  fim: string;
+};
+export type Ponto =
+  | "Letras"
+  | "ICB"
+  | "ICE"
+  | "IAD"
+  | "Engenharia"
+  | "CRITT"
+  | "RU"
+  | "CBR"
+  | "ICB"
+  | "FAEFID"
+  | "Direito"
+  | "HU"
+  | "Curva Repressa"
+  | "Corpo de Bombeiros"
+  | "Odonto"
+  | "Economia"
+  | "Educação"
+  | "ICH";
+export const AnelViario: Rotas = {
+  nome: "Anel Viário",
+  pontos: [
+    "Letras",
+    "ICB",
+    "ICE",
+    "IAD",
+    "Engenharia",
+    "RU",
+    "CRITT",
+    "CBR",
+    "ICB",
+    "FAEFID",
+    "Curva Repressa",
+    "Corpo de Bombeiros",
+    "Direito",
+  ],
+};
+export const Circular: Rotas = {
+  nome: "Circular",
+  pontos: [
+    "Letras",
+    "ICB",
+    "ICE",
+    "IAD",
+    "Engenharia",
+    "RU",
+    "CRITT",
+    "CBR",
+    "ICB",
+    "FAEFID",
+    "HU",
+    "Corpo de Bombeiros",
+    "Odonto",
+    "Economia",
+    "Educação",
+    "ICH",
+    "Direito",
+  ],
+};
+export const OdontoDireto: Rotas = {
+  nome: "Odonto-Direto",
+  pontos: [
+    "Letras",
+    "ICB",
+    "ICE",
+    "IAD",
+    "Engenharia",
+    "RU",
+    "CRITT",
+    "CBR",
+    "ICB",
+    "FAEFID",
+    "Curva Repressa",
+    "Corpo de Bombeiros",
+    "Odonto",
+    "Economia",
+    "Educação",
+    "Direito",
+  ],
+};
+export const OdontoIchRU: Rotas = {
+  nome: "Odonto-ICH-RU",
+  pontos: [
+    "Letras",
+    "ICB",
+    "ICE",
+    "IAD",
+    "Engenharia",
+    "RU",
+    "CRITT",
+    "CBR",
+    "ICB",
+    "FAEFID",
+    "Curva Repressa",
+    "Corpo de Bombeiros",
+    "Odonto",
+    "Economia",
+    "Educação",
+    "ICH",
+    "Direito",
+  ],
+};
+
+export const HURUDireto: Rotas = {
+  nome: "HU-RU direto",
+  pontos: [
+    "Letras",
+    "ICB",
+    "ICE",
+    "IAD",
+    "Engenharia",
+    "RU",
+    "CRITT",
+    "CBR",
+    "ICB",
+    "FAEFID",
+    "HU",
+    "Corpo de Bombeiros",
+    "Direito",
+  ],
+};
