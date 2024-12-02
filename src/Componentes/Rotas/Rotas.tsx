279df447181a3f1cc7cb50daa1cc6419d9a5cd00
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-01 22:57:38 -0300

    adiciona fonte Roboto

--- src/Componentes/Rotas/Rotas.tsx
+++ src/Componentes/Rotas/Rotas.tsx
@@ -18,7 +18,7 @@ function Rotas() {
   }
   return (
       <div className='Rotas'>
-          <Header titulo='rotas' onchangeHandler={onSelectHandler} select={selectIPT} options={rotasNome}></Header>
+          <Header titulo='Rotas' onchangeHandler={onSelectHandler} select={selectIPT} options={rotasNome}></Header>
         <main>
           <div className='Pontos'>
                 <MapView rota={rota}></MapView>
