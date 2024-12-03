7fe0110ff0671756fd53f45942607deecb654b7e
Caio Dev <caioinnocencio@gmail.com>
2024-12-03 18:48:13 -0300

    Utilizando o React.FC para montar o componente: ChatInput.

--- src/Componentes/Chat/ChatImput.tsx
+++ src/Componentes/Chat/ChatImput.tsx
@@ -4,3 +4,20 @@ import { sendMessage } from "../../APIS/firebaseAPI";
 interface ChatInputProps {
   user: string;
 }
+
+const ChatInput: React.FC<ChatInputProps> = ({ user }) => {
+    const [message, setMessage] = useState<string>("");
+  
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      if (!message.trim()) return;
+  
+      try {
+        await sendMessage(user, message);
+        setMessage(""); // Limpa o campo de entrada após o envio
+      } catch (error) {
+        console.error("Erro ao enviar a mensagem:", error);
+      }
+    };
+
+}
