aa3294b235c4e248cce18225e3a0c1a5b5407137
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-06 17:55:52 -0300

    adiciona conteudo

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,27 @@
 
+import Header from '../../Componentes/HeaderInputs/header'
 import './Home.css'
 
 
 function Home() {
 
   return (
-    
     <div className='Home'>
-       <p>Seja Bem Vindo!</p>
+
+      <Header titulo='Home'>{null}</Header>
+      <main>
+        <h1>Bem-vindo!</h1>
+        <p>Este projeto foi desenvolvido para simplificar e interpretar informações sobre o Circular da UFJF, promovendo mais praticidade e bem-estar para a comunidade acadêmica como um todo.</p>
+        <p>Na aba <strong>Previsões</strong>, você pode consultar os horários previstos dos ônibus, organizados de acordo com o ponto de sua escolha.</p>
+        <p>Na aba <strong>Rotas</strong>, você encontrará uma lista das rotas atuais, além de sua visualização em um mapa dinâmico e interativo.</p>
+        <p>Este é um projeto de código aberto, sem fins lucrativos, baseado exclusivamente em informações públicas fornecidas pela UFJF, disponíveis no site oficial: <a href="https://www2.ufjf.br/transporte/veiculos/onibus-circular/" target="_blank">https://www2.ufjf.br/transporte/veiculos/onibus-circular/</a>.</p>
+        <p>
+          Idealizado por: <a href='https://github.com/Miguel-Dias-112/'>Miguel Dias</a> e <a href='https://github.com/TheDevCaio'>Caio Machado</a>
+
+        </p>
+      </main>
+
+      
     </div>
       
    
