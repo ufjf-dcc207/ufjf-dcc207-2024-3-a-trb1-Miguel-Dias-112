1d1145871edb48a66ecc1a4f6bbdec7585e90c38
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-12-16 13:51:18 -0300

    configura base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/CadeCircularUfjf",
+  base:"/ufjf-dcc207-2024-3-a-trb1-Miguel-Dias-112",
   plugins: [react()],
 })
